7668c7a62c112a7a6ab474cbb3a3138bbceb4b4e
Reuben Greaves <reuben@plural.ai>
2025-05-01 12:15:40 +0100

    Adding a billing API

--- infra/storage.ts
+++ infra/storage.ts
@@ -1,5 +1,8 @@
 export const bucket = new sst.aws.Bucket("Uploads");
 
+// Create a secret for Stripe
+export const secret = new sst.Secret("StripeSecretKey");
+
 // Create the DynamoDB table
 export const table = new sst.aws.Dynamo("Notes", {
   fields: {
